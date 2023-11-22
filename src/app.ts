b67517790c7d4f102935b5924fbbefc90c068e1e
matheus.s.pinheiro@live.com <matheus.s.pinheiro@live.com>
2023-11-22 14:17:07 -0300

    fix:added-cors-middleware

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import { appRoutes } from "./http/routes";
+import cors from "cors";
 
 export const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(appRoutes);
